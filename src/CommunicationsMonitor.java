9d66acab5ac81998a1af7e0a88d0a38cb33d70c5
Vignesh Krishnan <vigneshk@iastate.edu>
2019-04-22 19:59:34 -0500

    Added function templates

--- src/CommunicationsMonitor.java
+++ src/CommunicationsMonitor.java
@@ -1,4 +1,23 @@
+import java.util.List;
 
 public class CommunicationsMonitor {
 
+	public CommunicationsMonitor() {
+		
+	}
+	
+	public void addCommunication(int c1, int c2, int timestamp) {
+		
+	}	
+	
+	public void createGraph() {
+		
+	}
+	
+	public List<ComputerNode> queryInfection(int c1, int c2, int x, int y) {
+		return null;
+	}
+	
+	
+	
 }
--- src/ComputerNode.java
+++ src/ComputerNode.java
@@ -1,4 +1,32 @@
+import java.util.List;
 
 public class ComputerNode {
-
+	
+	int id;
+	int timestamp;
+	List<ComputerNode> neighbors;
+	
+	public ComputerNode(int id, int timestamp) {
+		this.id = id;
+		this.timestamp = timestamp;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public int getTimestamp() {
+		return timestamp;
+	}
+	
+	public List<ComputerNode> getOurNeighbors() {
+		return neighbors;
+	}
+	
+	
+	
 }
+
+
+
+
