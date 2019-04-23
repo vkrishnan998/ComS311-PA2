ca2e681a8aa8d97f57ab8f070ddc0470585df55f
Vignesh Krishnan <vigneshk@iastate.edu>
2019-04-22 21:52:45 -0500

    Add communications method

--- src/CommunicationsMonitor.java
+++ src/CommunicationsMonitor.java
@@ -1,13 +1,35 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class CommunicationsMonitor {
 
+	HashMap<Integer, List<ComputerNode>> computerMapping;
+	
+	
 	public CommunicationsMonitor() {
-		
+		computerMapping = new HashMap<Integer, List<ComputerNode>>();
 	}
 	
 	public void addCommunication(int c1, int c2, int timestamp) {
+		
+		if (!computerMapping.containsKey(c1)) {
+			ArrayList<ComputerNode> list = new ArrayList<ComputerNode>();
+			list.add(new ComputerNode(c1, timestamp));
+			computerMapping.put(c1, list);
+		}
+		else {
+			computerMapping.get(c1).add(new ComputerNode(c1, timestamp));
+		}
+		
+		if (!computerMapping.containsKey(c2)) {
+			ArrayList<ComputerNode> list = new ArrayList<ComputerNode>();
+			list.add(new ComputerNode(c1, timestamp));
+			computerMapping.put(c2, list);
+		}
+		else {
+			computerMapping.get(c2).add(new ComputerNode(c2, timestamp));
+		}
 		
 	}	
 	
@@ -20,11 +42,11 @@ public class CommunicationsMonitor {
 	}
 	
 	public HashMap<Integer, List<ComputerNode>> getComputerMapping() {
-		return null;
+		return computerMapping;
 	}
 	
 	public List<ComputerNode> getComputerMapping(int c) {
-		return null;
+		return computerMapping.get(c);
 	}
 	
 	
